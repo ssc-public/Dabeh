26320df6bfef3f5bde95d911dddf022c504c9999
alizademhdi <alizademhdi@gmail.com>
2023-12-20 15:29:28 +0330

    Add some new fields to receipt

--- financial/models.py
+++ financial/models.py
@@ -5,10 +5,11 @@ class Event(models.Model):
     date = models.DateField(auto_now=True)
 
 
-
 class Receipt(models.Model):
     summary = models.TextField(max_length=256)
     event = models.ForeignKey(to='financial.Event', on_delete=models.CASCADE)
     amount = models.IntegerField(help_text='with Toman units')
     owner = models.CharField(max_length=32)
     date = models.DateField(auto_now=True)
+    settled = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='receipts/', null=True)
