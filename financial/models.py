9ef04e764091f2f479ff6016c8bea267c8e1e8c6
alizademhdi <alizademhdi@gmail.com>
2023-12-20 15:22:21 +0330

    Add financial app

---
+++ financial/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Event(models.Model):
+    name = models.CharField(max_length=64, blank=False, null=False)
+    date = models.DateField(auto_now=True)
+
+
+
+class Receipt(models.Model):
+    summary = models.TextField(max_length=256)
+    event = models.ForeignKey(to='financial.Event', on_delete=models.CASCADE)
+    amount = models.IntegerField(help_text='with Toman units')
+    owner = models.CharField(max_length=32)
+    date = models.DateField(auto_now=True)
